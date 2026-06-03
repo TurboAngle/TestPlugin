badb62b86f40313685d02eb22118a0e2c35e5001
taika <taika@example.com>
2026-06-03 16:41:29 +0900

    test classの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    compileOnly("org.geysermc.floodgate:api:2.2.5-SNAPSHOT")
 }
 
 java {
@@ -47,17 +48,31 @@ version = "0.0.0"
 
 // ===== Custom Deploy Configurations =====
 val pluginName = project.name
+val targetDirFile = file("C:/Users/taika/mcss/servers/rel_test/plugins")
 
 tasks.jar {
     // Removed the build number variable, naming it standard plugin-version.jar
     archiveFileName.set("$pluginName-${project.version}.jar")
 }
 
+tasks.register("cleanOldPlugin") {
+    doLast {
+        val dir = targetDirFile
+        dir.listFiles()?.forEach { file ->
+            if (file.name.startsWith(pluginName) && file.name.endsWith(".jar")) {
+                println("Deleting old plugin: ${file.name}")
+                file.delete()
+            }
+        }
+    }
+}
 
 tasks.register<Copy>("deployPlugin") {
     dependsOn(tasks.jar)
+    dependsOn("cleanOldPlugin")
 
     from(tasks.jar)
+    into(targetDirFile)
 }
 
 tasks.build {
